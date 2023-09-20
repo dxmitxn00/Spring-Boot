6768fdd03701d1edda04c3e1c851af192b45f7ed
Tan Dami <95688240+dxmitxn00@users.noreply.github.com>
2023-09-20 09:55:45 +0900

    day92 : day90 답안

---
+++ day92/day90_answer/src/test/java/com/tan/app/Day90AnswerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tan.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day90AnswerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
